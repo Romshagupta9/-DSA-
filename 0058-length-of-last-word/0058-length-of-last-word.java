a9e6e706946e46022372f92f60813ca7a19db8da
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2025-04-21 11:23:02 +0530

    Time: 0 ms (100%), Space: 41.9 MB (48.04%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,21 +1,8 @@
-// class Solution {
-//     public int lengthOfLastWord(String s) {
-        
-//     }
-// }
 
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim();
-        int length = 0;
-        for(int i = s.length() - 1; i >= 0; i--){
-            if(s.charAt(i) != ' '){
-                length++;
-            }
-            else{
-                break;
-            }
-        }
-        return length;
+     String [] str=s.split(" ");
+     int n=str.length;
+     return str[n-1].length();
     }
 }
