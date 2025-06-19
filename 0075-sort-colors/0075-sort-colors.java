dd402e928e2234efc2de2244e13b4744f70839a6
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2025-06-19 16:18:57 +0530

    Time: 0 ms (100%), Space: 41.8 MB (92.59%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -8,26 +8,26 @@ class Solution {
 // nums[mid] == 1 ➝ 1 mila: kuch mat karo, bas mid aage badhao.
 // nums[mid] == 2 ➝ 2 mila: mid aur high ki value swap karo, sirf high ko peeche le jao (kyunki mid pe aayi nayi value ko check karna zaroori hai).
 
-        int l=0;
-        int m=0;
-        int h=arr.length-1;
-        while(m<=h){
-             if(arr[m]==0){
-                int temp=arr[l];
-                arr[l]=arr[m];
-                arr[m]=temp;
-                l++;
-                m++;
-             }
-             else if(arr[m]==1){
-                m++;
-             }
-             else{
-                int temp=arr[m];
-                arr[m]=arr[h];
-                arr[h]=temp;
-                h--;
-             }
+       int l=0;
+       int m=0;
+       int h=arr.length-1;
+       while(m<=h){
+        if(arr[m]==0){
+            int temp=arr[l];
+            arr[l]=arr[m];
+            arr[m]=temp;
+            l++;
+            m++;
         }
+        else if(arr[m]==1){
+            m++;
+        }
+        else if(arr[m]==2){
+            int temp=arr[m];
+            arr[m]=arr[h];
+            arr[h]=temp;
+            h--;
+        }
+       }
     }
 }
