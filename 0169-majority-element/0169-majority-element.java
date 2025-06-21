06701db2837033c4e95ff7818c065481f2d2b619
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2025-06-21 16:11:34 +0530

    Time: 1 ms (99.84%), Space: 53.1 MB (59.62%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        // int max=0,index=0;
+        // int [] count=new int[10001];
+        // for(int i=0;i<nums.length;i++){
+        //     count[nums[i]]++;
+        // }
+        // for(int i=0;i<count.length;i++){
+        //     if(count[i]>=max){
+        //         max=count[i];
+        //         index=i;
+        //     }
+        // }
+        // return index;
+        int count = 0;
+        int candidate = 0;
+        
+for (int i = 0; i < nums.length; i++) {
+    if (count == 0) {
+        candidate = nums[i];
+    }
+
+    if (nums[i] == candidate) {
+        count++;
+    } else {
+        count--;
+    }
+}
+return candidate;
+}
+}
