12e13123b24d3e9bd054a08c4b7abc2f4cce70b0
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2025-07-09 09:25:11 +0530

    Time: 2 ms (76.11%), Space: 63.6 MB (21.11%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.java
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.java
@@ -1,23 +1,14 @@
 class Solution {
-  public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
-    int[] gaps = getGaps(eventTime, startTime, endTime);
-    int windowSum = Arrays.stream(gaps, 0, k + 1).sum();
-    int ans = windowSum;
-
-    for (int i = k + 1; i < gaps.length; i++) {
-      windowSum += gaps[i] - gaps[i - k - 1];
-      ans = Math.max(ans, windowSum);
+    int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
+    int[] gap = new int[startTime.length +1];
+    gap[0] = startTime[0];
+    for(int i = 1; i < startTime.length; ++i) gap[i] = startTime[i] -  endTime[i-1];
+    gap[startTime.length] = eventTime - endTime[endTime.length-1];
+    int ans = 0, sum = 0;
+    for(int i = 0; i < gap.length; ++i){
+        sum += gap[i] - ((i >=  k+1)?gap[i - (k+1)]:0);
+        ans = Math.max(ans, sum);
     }
-
     return ans;
-  }
-
-  private int[] getGaps(int eventTime, int[] startTime, int[] endTime) {
-    int[] gaps = new int[startTime.length + 1];
-    gaps[0] = startTime[0];
-    for (int i = 1; i < startTime.length; ++i)
-      gaps[i] = startTime[i] - endTime[i - 1];
-    gaps[startTime.length] = eventTime - endTime[endTime.length - 1];
-    return gaps;
-  }
+}
 }
