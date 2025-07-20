56c1e8199308c32e6cececc1dc36b66f2fd2956d
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2025-07-20 14:06:03 +0530

    Time: 67 ms (97.73%), Space: 76.6 MB (25%) - LeetHub

--- 2079-delete-duplicate-folders-in-system/2079-delete-duplicate-folders-in-system.java
+++ 2079-delete-duplicate-folders-in-system/2079-delete-duplicate-folders-in-system.java
@@ -1,70 +1,64 @@
-class TrieNode {
-  public Map<String, TrieNode> children = new HashMap<>();
-  public boolean deleted = false;
-}
-
 class Solution {
-  public List<List<String>> deleteDuplicateFolder(List<List<String>> paths) {
-    List<List<String>> ans = new ArrayList<>();
-    Map<String, List<TrieNode>> subtreeToNodes = new HashMap<>();
+    class Node {
+        Map<String, Node> subNodes = new TreeMap<>();
 
-    Collections.sort(paths,
-                     Comparator.<List<String>>comparingInt(List::size).thenComparing((a, b) -> {
-                       for (int i = 0; i < Math.min(a.size(), b.size()); ++i) {
-                         final int c = a.get(i).compareTo(b.get(i));
-                         if (c != 0)
-                           return c;
-                       }
-                       return 0;
-                     }));
+        String content = "";
 
-    for (List<String> path : paths) {
-      TrieNode node = root;
-      for (final String s : path) {
-        node.children.putIfAbsent(s, new TrieNode());
-        node = node.children.get(s);
-      }
+        boolean remove = false;
+
+        void markRemove() {
+            if (remove) {
+                return;
+            }
+            remove = true;
+            if (subNodes != null) {
+                for (Node value : subNodes.values()) {
+                    value.markRemove();
+                }
+            }
+        }
     }
 
-    buildSubtreeToRoots(root, subtreeToNodes);
-
-    for (List<TrieNode> nodes : subtreeToNodes.values())
-      if (nodes.size() > 1)
-        for (TrieNode node : nodes)
-          node.deleted = true;
-
-    constructPath(root, new ArrayList<>(), ans);
-    return ans;
-  }
-
-  private TrieNode root = new TrieNode();
-
-  private StringBuilder buildSubtreeToRoots(TrieNode node,
-                                            Map<String, List<TrieNode>> subtreeToNodes) {
-    StringBuilder sb = new StringBuilder("(");
-    for (final String s : node.children.keySet()) {
-      TrieNode child = node.children.get(s);
-      sb.append(s).append(buildSubtreeToRoots(child, subtreeToNodes));
+    public List<List<String>> deleteDuplicateFolder(List<List<String>> paths) {
+        paths.sort(Comparator.comparingInt(List::size));
+        List<Node> nodes = new ArrayList<>(paths.size());
+        Node rootNode = new Node();
+        for (List<String> pathList : paths) {
+            Node current = rootNode;
+            int last = pathList.size() - 1;
+            for (int i = 0; i < last; i++) {
+                String s = pathList.get(i);
+                current = current.subNodes.get(s);
+            }
+            String name = pathList.get(last);
+            Node node = new Node();
+            current.subNodes.put(name, node);
+            nodes.add(node);
+        }
+        StringBuilder content = new StringBuilder();
+        Map<String, Node> nodeByContent = new HashMap<>();
+        for (int i = nodes.size() - 1; i >= 0; i--) {
+            Node node = nodes.get(i);
+            if (node.subNodes.isEmpty()) {
+                continue;
+            }
+            for (Map.Entry<String, Node> entry : node.subNodes.entrySet()) {
+                content.append(entry.getKey()).append('{').append(entry.getValue().content).append('}');
+            }
+            node.content = content.toString();
+            content.delete(0, content.length());
+            Node similar = nodeByContent.putIfAbsent(node.content, node);
+            if (similar != null) {
+                node.markRemove();
+                similar.markRemove();
+            }
+        }
+        List<List<String>> ans = new ArrayList<>();
+        for (int i = 0; i < paths.size(); i++) {
+            if (!nodes.get(i).remove) {
+                ans.add(paths.get(i));
+            }
+        }
+        return ans;
     }
-    sb.append(")");
-    final String subtree = sb.toString();
-    if (!subtree.equals("()")) {
-      subtreeToNodes.putIfAbsent(subtree, new ArrayList<>());
-      subtreeToNodes.get(subtree).add(node);
-    }
-    return sb;
-  }
-
-  private void constructPath(TrieNode node, List<String> path, List<List<String>> ans) {
-    for (final String s : node.children.keySet()) {
-      TrieNode child = node.children.get(s);
-      if (!child.deleted) {
-        path.add(s);
-        constructPath(child, path, ans);
-        path.remove(path.size() - 1);
-      }
-    }
-    if (!path.isEmpty())
-      ans.add(new ArrayList<>(path));
-  }
 }
