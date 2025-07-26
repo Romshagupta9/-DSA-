27974bfbe563520c4897adb6f1b1dcee65385999
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2025-07-26 14:20:45 +0530

    Time: 68 ms (46.15%), Space: 127.5 MB (30.77%) - LeetHub

--- 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.java
+++ 3789-maximize-subarrays-after-removing-one-conflicting-pair/3789-maximize-subarrays-after-removing-one-conflicting-pair.java
@@ -1,46 +1,36 @@
 class Solution {
-  public long maxSubarrays(int n, int[][] conflictingPairs) {
-    long validSubarrays = 0;
-    int maxLeft = 0;
-    int secondMaxLeft = 0;
-    // gains[i] := the number of additional valid subarrays we can gain if the
-    // restriction at index `i` is removed
-    long[] gains = new long[n + 1];
-    // conflicts[r] := left endpoints that conflict with subarrays ending in r
-    List<Integer>[] conflicts = new List[n + 1];
-
-    for (int i = 0; i <= n; ++i)
-      conflicts[i] = new ArrayList<>();
-
-    for (int[] pair : conflictingPairs) {
-      final int a = pair[0];
-      final int b = pair[1];
-      conflicts[Math.max(a, b)].add(Math.min(a, b));
-    }
-
-    for (int right = 1; right <= n; ++right) {
-      for (int left : conflicts[right]) {
-        if (left > maxLeft) {
-          secondMaxLeft = maxLeft;
-          maxLeft = left;
-        } else if (left > secondMaxLeft) {
-          secondMaxLeft = left;
+    public long maxSubarrays(int n, int[][] conflictingPairs) {
+        List<List<Integer>> conflicts = new ArrayList<>();
+        for(int i=0 ; i<=n ; i++)
+            conflicts.add(new ArrayList<>());
+        
+        for(int[] c : conflictingPairs) {
+            int left = c[0], right = c[1];
+            if(left>right) {
+                int temp = left;
+                left = right;
+                right = temp;
+            }
+            conflicts.get(right).add(left);
         }
-      }
-      // Subarrays [maxLeft + 1..right],
-      //           [maxLeft + 2..right],
-      //           ...
-      //           [right..right] are valid.
-      validSubarrays += right - maxLeft;
-      // If we remove `maxLeft` (the most restrictive conflict), we gain
-      // `maxLeft - secondMaxLeft` new subarrays:
-      // [secondMaxLeft + 1..right],
-      // [secondMaxLeft + 2..right],
-      // ...
-      // [maxLeft..right].
-      gains[maxLeft] += maxLeft - secondMaxLeft;
+        long[] restrictRemoval = new long[n+1];
+        int leftMaxRestrict = 0, leftSecondMaxRestrict = 0;
+        long res = 0L;
+        for(int i=1 ; i<=n ; i++) {
+            for(Integer ele : conflicts.get(i)) {
+                if(ele > leftMaxRestrict) {
+                    leftSecondMaxRestrict = leftMaxRestrict;
+                    leftMaxRestrict = ele;
+                } else if(ele > leftSecondMaxRestrict)
+                    leftSecondMaxRestrict = ele;
+            }
+            res += 0L + i - leftMaxRestrict;
+            restrictRemoval[leftMaxRestrict] += leftMaxRestrict - leftSecondMaxRestrict;
+        }
+        long maxRemovalVal = 0L;
+        for(int i=1 ; i<=n ; i++)
+            maxRemovalVal = Math.max(maxRemovalVal, restrictRemoval[i]);
+        res += maxRemovalVal;
+        return res;
     }
-
-    return validSubarrays + Arrays.stream(gains).max().getAsLong();
-  }
 }
