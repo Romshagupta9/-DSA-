39f4aa7cc92578e4e40b9c3170f111452ed44436
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2025-07-28 10:27:53 +0530

    Time: 12 ms (36.86%), Space: 49.7 MB (80.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -14,23 +14,38 @@ class Solution {
         // return index;
 
 
-       int count=0;
-       int vip=0;
-       for(int i=0;i<nums.length;i++){
-        if(count==0){
-            vip=nums[i];
-        }
-        if(nums[i]==vip){
-           count++;
-        }
-        else{
-            count--;
-        }
-       }
-       return vip;
+    //    int count=0;
+    //    int vip=0;
+    //    for(int i=0;i<nums.length;i++){
+    //     if(count==0){
+    //         vip=nums[i];
+    //     }
+    //     if(nums[i]==vip){
+    //        count++;
+    //     }
+    //     else{
+    //         count--;
+    //     }
+    //    }
+    //    return vip;
 
-        // Arrays.sort(nums);
-        // int n = nums.length;
-        // return nums[n/2];
+
+    // Arrays.sort(nums);
+    // int n=nums.length;
+    // return nums[n/2];
+
+
+    HashMap<Integer,Integer> hm= new HashMap<>();
+    for(int i:nums){
+        hm.put(i,hm.getOrDefault(i,0)+1);
+    }
+    int max=0,majority=0;
+    for(int i :hm.keySet()){
+        if(hm.get(i)>majority){
+            max=i;
+            majority=hm.get(i);
+        }
+    }
+    return max;
 }
 }
