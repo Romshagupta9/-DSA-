4da69af560bd6a761c3ac01b5779ea166d797e4b
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2025-07-30 15:30:59 +0530

    Time: 3 ms (82.57%), Space: 61.2 MB (29.46%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.java
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.java
@@ -1,31 +1,16 @@
 class Solution {
     public int longestSubarray(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
+        int ma = 0;
+        for(int i : nums)
+            ma = Math.max(ma , i);
+        int ans = 0 , curr = 0;
+        for(int i : nums) {
+            if(i==ma)
+                curr++;
+            else
+                curr = 0;
+            ans = Math.max(ans,curr);
         }
-
-        // 1. Find the maximum value in the array.
-        int maxVal = 0;
-        for (int num : nums) {
-            if (num > maxVal) {
-                maxVal = num;
-            }
-        }
-        
-        // 2. Find the length of the longest subarray of maxVal.
-        int maxLen = 0;
-        int currentLen = 0;
-        
-        for (int num : nums) {
-            if (num == maxVal) {
-                currentLen++;
-            } else {
-                maxLen = Math.max(maxLen, currentLen);
-                currentLen = 0;
-            }
-        }
-        
-        // 3. Final check for a trailing streak.
-        return Math.max(maxLen, currentLen);
+        return ans;
     }
 }
