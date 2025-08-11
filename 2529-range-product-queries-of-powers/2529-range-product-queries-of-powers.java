d12d4aba63ba6b2ad85560339aceaba163bf6d77
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2025-08-11 09:33:22 +0530

    Time: 20 ms (60.22%), Space: 93.4 MB (16.13%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.java
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.java
@@ -1,23 +1,34 @@
 class Solution {
+    final int MOD = 1_000_000_007;
+
     public int[] productQueries(int n, int[][] queries) {
-        return ((java.util.function.Function<java.util.List<Integer>, int[]>) a -> {
-            int MOD = 1_000_000_007;
-            int[] res = new int[queries.length];
-            for (int i = 0; i < queries.length; i++) {
-                int left = queries[i][0];
-                int right = queries[i][1];
-                res[i] = (int) (Math.pow(2, a.get(right + 1) - a.get(left)) % MOD);
+        // Step 1: extract the powers of 2 that sum to n
+        List<Long> powers = new ArrayList<>();
+        int bit = 0;
+        int temp = n;
+        while (temp > 0) {
+            if ((temp & 1) == 1) {
+                powers.add((1L << bit) % MOD); // safe bit shift
             }
-            return res;
-        }).apply(
-            new java.util.ArrayList<Integer>() {{
-                add(0);
-                for (int i = 0; i < 31; i++) {
-                    if (((n >> i) & 1) == 1) {
-                        add(get(size() - 1) + i);
-                    }
-                }
-            }}
-        );
+            bit++;
+            temp >>= 1;
+        }
+        powers.sort(Long::compareTo);
+
+        // Step 2: answer queries by multiplying in the range
+        int q = queries.length;
+        int[] result = new int[q];
+
+        for (int i = 0; i < q; i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+            long prod = 1;
+            for (int j = start; j <= end; j++) {
+                prod = (prod * powers.get(j)) % MOD;
+            }
+            result[i] = (int) prod;
+        }
+
+        return result;
     }
 }
