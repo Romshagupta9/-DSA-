024fee74e56651ec90b5a48ce7199fd5148cbce6
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2025-10-06 10:22:08 +0530

    Time: 19 ms (48.71%), Space: 45.5 MB (20.48%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,28 +1,21 @@
 class Solution {
     public String longestPalindrome(String s) {
-        //comparisons
-        //odd : i+1 and i-1
-        //even : i and i+1
-        if(s.length()<=1){
-            return s;
-        }
-        String maxstr=s.substring(0,1);
-        for(int i=0;i<s.length()-1;i++){
+        String maxstr = s.substring(0,1);
+        for(int i=0;i<s.length();i++){
             String odd = ok(s,i,i);
-            String even = ok(s,i,i+1);
-
-            if(odd.length()>maxstr.length()){
+            String even= ok(s,i,i+1);
+            if(maxstr.length()<odd.length()){
                 maxstr=odd;
             }
-            if(even.length()>maxstr.length()){
+            if(maxstr.length()<even.length()){
                 maxstr=even;
             }
         }
         return maxstr;
     }
     private String ok(String s,int l,int r){
-        while(l>=0 && r<s.length() &&s.charAt(l)==s.charAt(r)){
-            l--;
+        while(l>=0 && r<s.length() && s.charAt(l)==s.charAt(r)){
+            l--;  //expand 
             r++;
         }
         return s.substring(l+1,r);
