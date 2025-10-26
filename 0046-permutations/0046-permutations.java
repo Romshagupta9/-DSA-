22395cf8b100a6bc4c0ffa92abc9963b7bd6b88a
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2025-10-26 15:28:46 +0530

    Time: 1 ms (93.02%), Space: 44.6 MB (65.42%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -3,20 +3,19 @@ class Solution {
         List<List<Integer>> res=new ArrayList<>();
         List <Integer> ls=new ArrayList<>();
         boolean [] used=new boolean[nums.length];
-        helper(nums,res,ls,0,used);
+        helper(nums,res,ls,used);
         return res;
     } 
-    private void helper(int[] nums,List<List<Integer>> res,List <Integer> ls,
-    int i,boolean [] used){
+    private void helper(int[] nums,List<List<Integer>> res,List <Integer> ls,boolean [] used){
         if(ls.size()==nums.length){
             res.add(new ArrayList<>(ls));
             return;
         }
-        for(i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++){
             if(used[i]==false){
                 used[i]=true;
                 ls.add(nums[i]);
-                helper(nums,res,ls,i+1,used);
+                helper(nums,res,ls,used);
                 ls.remove(ls.size()-1);
                 used[i]=false;
             }
