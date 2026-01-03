61043ecbd97a25580c521acff5ae573eb0122a10
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2026-01-03 17:49:23 +0530

    Time: 0 ms (100%), Space: 43.5 MB (51.14%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,10 +1,7 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        List<String> ls= new ArrayList<>();
-        if(digits.length()==0){
-            return ls;
-        }
-        HashMap <Character,String> hm= new HashMap<>();
+        List<String> ls=new ArrayList<>();
+        HashMap<Character,String> hm=new HashMap<>();
         hm.put('2',"abc");
         hm.put('3',"def");
         hm.put('4',"ghi");
@@ -13,21 +10,20 @@ class Solution {
         hm.put('7',"pqrs");
         hm.put('8',"tuv");
         hm.put('9',"wxyz");
-
-        helper(hm,digits,new StringBuilder(),ls,0);
+        helper(digits,hm,ls,new StringBuilder(),0);
         return ls;
-    }
-
-    public void helper(HashMap<Character,String> hm,String digits,StringBuilder sb,List<String> ls,int i){
+    }    
+    public void helper(String digits,HashMap<Character,String> hm
+    ,List<String> ls,StringBuilder sb,int i){
         if(i==digits.length()){
             ls.add(sb.toString());
-            return ;
+            return;
         }
         String curr=hm.get(digits.charAt(i));
         for(int k=0;k<curr.length();k++){
             sb.append(curr.charAt(k));
-            helper(hm,digits,sb,ls,i+1);
+            helper(digits,hm,ls,sb,i+1);
             sb.deleteCharAt(sb.length()-1);
-        }
+        } 
     }
-}
+} 
