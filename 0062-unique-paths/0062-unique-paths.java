5d63da8eb9ce9f5439392697831fb00ae694a3bc
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2026-02-02 19:42:52 +0530

    Time: 0 ms (100%), Space: 42.1 MB (60.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,8 +1,9 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        //using bottom up approach
-        int [][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
+//using bottom up approach : O(m*n);
+//we are movig bottom up , ham niche se chal rhe hia and dekh rhe hia ki us particular block---> (ki right or down se)--> destination tak pahuchne k kitne paths hoskte hai...
+        int [][]dp=new int[m][n];
+        for(int i=0; i<m;i++){
             for(int j=0;j<n;j++){
                 if(i==m-1 || j==n-1){
                     dp[i][j]=1;
