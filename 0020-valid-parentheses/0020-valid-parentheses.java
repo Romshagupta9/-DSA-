dc8db58e2ada0083cc213ac21773c9f9d6612fde
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2026-02-09 20:00:34 +0530

    Time: 4 ms (42.56%), Space: 43.6 MB (25.2%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,10 +1,10 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st= new Stack<>();
+        Stack<Character>st=new Stack<>();
         int i=0;
         while(i<s.length()){
             char ch=s.charAt(i);
-            if(ch=='(' || ch=='[' || ch=='{'){
+            if(ch=='{' || ch=='(' || ch=='['){
                 st.push(ch);
             }
             else{
@@ -12,11 +12,11 @@ class Solution {
                     return false;
                 }
                 char top=st.pop();
-                if(top=='{' && ch!='}'
-                || top=='(' && ch!=')'
-                || top=='[' && ch!=']'){
-                    return false;
-                }
+                if((top=='[' && ch!=']') ||
+                    (top=='(' && ch!=')') ||
+                    (top=='{' && ch!='}')){
+                        return false;
+                    }
             }
             i++;
         }
