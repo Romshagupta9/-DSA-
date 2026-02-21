12a11d4b4760d3d023653a7069944221a0cf0a11
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2026-02-21 18:43:40 +0530

    Time: 15 ms (61.91%), Space: 51.3 MB (87.58%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int count=0,i=0,j=0;
+        Arrays.sort(s);
+        Arrays.sort(g);
+        while(i<g.length && j<s.length){
+                if(s[j]>=g[i]){
+                    count++;
+                    i++;
+                }
+                j++;
+        }
+        return count;
+    }
+}
