f77c5685e5a8935b99cd4bde6b6c088e217e8f80
Romsha Gupta <157394956+Romshagupta9@users.noreply.github.com>
2026-05-23 15:29:33 +0530

    Time: 0 ms (100%), Space: 42.8 MB (89.95%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,15 +1,17 @@
+// count will store how many times the sorted order breaks.
+// In a rotated sorted array, the order should break at most once.
+
 class Solution {
     public boolean check(int[] nums) {
-         int n=nums.length;
-         int count=0;
-         for(int i=1;i<n;i++){
-            if (nums[i-1]>nums[i]){
+         int count=0,n=nums.length;
+        for(int i=1;i<n;i++){
+            if(nums[i-1]>nums[i]){
                 count++;
             }
-         }
-         if(nums[n-1]>nums[0]){
+        }
+        if(nums[n-1]>nums[0]){
             count++;
-         }
-         return count<=1;
+        }
+        return count<=1;
     } 
 }
